77263553b8ddeb8847c2f750f21c11aa3d43e8f4
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2023-12-19 01:34:30 +0400

    Initial commit

---
+++ obu/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"log"
+	"math"
+	"math/rand"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const wsEndPoint = "ws://localhost:30000/ws"
+
+var sendInterval = time.Second
+
+type OBUData struct {
+	OBUID int     `json:"obuID"`
+	Lat   float64 `json:"lat"`
+	Long  float64 `json:"long"`
+}
+
+// func sebdOBUData(conn *websocket.Conn, data OBUData) error {
+// 	return conn.WriteJSON(data)
+// }
+
+func genLatLong() (float64, float64) {
+	return genCoord(), genCoord()
+}
+
+func genCoord() float64 {
+	n := float64(rand.Intn(100) + 1)
+	f := rand.Float64()
+	return n + f
+}
+
+func main() {
+	obuIDS := generateOBUIDS(20)
+	conn, _, err := websocket.DefaultDialer.Dial(wsEndPoint, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		for i := 0; i < len(obuIDS); i++ {
+			lat, long := genLatLong()
+			data := OBUData{
+				OBUID: obuIDS[i],
+				Lat:   lat,
+				Long:  long,
+			}
+			if err := conn.WriteJSON(data); err != nil {
+				log.Fatal(err)
+			}
+			//fmt.Printf("%+v\n", data)
+		}
+
+		time.Sleep(sendInterval)
+	}
+	//fmt.Println(rand.New(rand.NewSource(time.Now().UnixNano())))
+
+}
+
+func generateOBUIDS(n int) []int {
+	ids := make([]int, n)
+	for i := 0; i < n; i++ {
+		ids[i] = rand.Intn(math.MaxInt)
+	}
+	return ids
+}
+
+func init() {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+}
