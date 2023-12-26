b1d9f8f55a7d5a9474a2d51bddb8c9b50c6687d8
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2023-12-26 11:30:12 +0400

    47 invoice agr der basladoq servisdeki sliceni refactoru

--- distance_calculator/middleware.go
+++ distance_calculator/middleware.go
@@ -1,12 +1,15 @@
 package main
 
-import "github.com/sadagatasgarov/toll-calc/types"
+import (
+	"time"
+
+	"github.com/sadagatasgarov/toll-calc/types"
+	"github.com/sirupsen/logrus"
+)
 
 type LogMiddleware struct {
 	next CalculatorServicer
 }
-
-
 
 func NewLogMiddleware(next CalculatorServicer) CalculatorServicer {
 	return &LogMiddleware{
@@ -15,5 +18,15 @@ func NewLogMiddleware(next CalculatorServicer) CalculatorServicer {
 }
 
 func (m *LogMiddleware) CalculateDistance(data types.OBUData) (float64, error) {
+	dist, err := m.next.CalculateDistance(data)
+
+	defer func(start time.Time) {
+		logrus.WithFields(logrus.Fields{
+			"took": time.Since(start),
+			"err":  err,
+			"dist": dist,
+		}).Info("calculate distance")
+	}(time.Now())
+
 	return 0.0, nil
 }
